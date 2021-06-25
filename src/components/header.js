119857c0fad2fdc491539c906fd8e7ebd7960051
Luiggy Macias <ferrinluiggy@gmail.com>
2021-06-25 16:57:18 -0500

    updated

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //css
 import './css/header.css';
 //images
@@ -7,16 +7,51 @@ import desktop_image_2 from './images/desktop-image-hero-2.jpg';
 import desktop_image_3 from './images/desktop-image-hero-3.jpg';
 import mobile_image_1 from './images/mobile-image-hero-1.jpg';
 import mobile_image_2 from './images/mobile-image-hero-2.jpg';
+import mobile_image_3 from './images/mobile-image-hero-3.jpg';
 import logo from './images/logo.svg';
 import burger from './images/icon-hamburger.svg';
 import close from './images/icon-close.svg';
 
 
 const Header = () => {
-	const [value, setValue] = React.useState('none');
+    let active = {
+        active1: 'active',
+        active2: '',
+        active3: '',
+    }
+    const [value, setValue] = React.useState('none');
+    const [index, setIndex] = useState(0);
 
-	
-	return <header>
+    const handleClickPrev = () => {
+    	console.log(`estas en el index ${index}`)
+        setIndex(index - 1)
+        if (index < 1) {
+            setIndex(2)
+            active = {
+                active1: '',
+                active2: '',
+                active3: 'active',
+            }
+        }
+        if (index > 2) {
+        	setIndex(0)
+            active = {
+                active1: 'active',
+                active2: '',
+                active3: '',
+            }
+        }
+        if (index === 1) {
+            active = {
+                active1: '',
+                active2: 'active',
+                active3: '',
+            }
+        }
+        console.log(active)
+    }
+    useEffect(() => {}, [])
+    return <header>
 		<div className='nav_container'>
 		 	<figure className='burger_and_logo_container'>
 		 		<img src={burger} alt="" className='burger' onClick={() => setValue('flex')}/>
@@ -35,11 +70,12 @@ const Header = () => {
 		    </nav>
 		</div>
 		<div className='images_container'>
-			<img src={mobile_image_1} alt="" className='img1 active'/>
-			<img src={mobile_image_2} alt="" className='img2 '/>
+			<img src={mobile_image_1} alt="" className={`img ${active.active1}`}/>
+			<img src={mobile_image_2} alt="" className={`img ${active.active2}`}/>
+			<img src={mobile_image_3} alt="" className={`img ${active.active3}`}/>
 			<div className='arrows_container'>
-				<div className='arrow left'></div>
-				<div className='arrow right'></div>
+				<div className='arrow left' onClick={handleClickPrev}></div>
+				<div className='arrow right' ></div>
 			</div>
 		</div>
 	</header>
