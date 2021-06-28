cf16fcf44f2228c4a34fd46b9574f2e147b68220
Luiggy Macias <ferrinluiggy@gmail.com>
2021-06-28 11:30:12 -0500

    button shop now

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,7 @@ import mobile_image_3 from './images/mobile-image-hero-3.jpg';
 import logo from './images/logo.svg';
 import burger from './images/icon-hamburger.svg';
 import close from './images/icon-close.svg';
+import arrow from './images/icon-arrow.svg';
 
 
 const Header = () => {
@@ -60,7 +61,9 @@ const Header = () => {
             <p>We provide unmatched quality, comfort, and style for property owners across the country.
                 Our experts combine form and function in bringing your vision to life. Create a room in your
                 own style with our collection and make your property a reflection of you and what you love.</p>
-            <button>Shop now</button>
+            <div className='button_container'>
+            	<button>Shop now</button><svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero" className='svg_arrow'/></svg>
+            </div>
         </div>
 	</header>
 }
