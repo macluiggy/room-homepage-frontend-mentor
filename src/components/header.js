d51a905c5ca336f5184a58033d141444127de773
Luiggy Macias <ferrinluiggy@gmail.com>
2021-06-28 14:47:05 -0500

    starting the desktop version

--- src/components/header.js
+++ src/components/header.js
@@ -62,7 +62,7 @@ const Header = () => {
                 Our experts combine form and function in bringing your vision to life. Create a room in your
                 own style with our collection and make your property a reflection of you and what you love.</p>
             <div className='button_container'>
-            	<button>Shop now</button><svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero" className='svg_arrow'/></svg>
+            	<button>Shop now</button><img src={arrow} alt="right incon arrow" />
             </div>
         </div>
 	</header>
