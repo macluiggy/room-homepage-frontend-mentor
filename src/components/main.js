0a97371162322884c36a90f09f3bb850acf8540f
Luiggy Macias <ferrinluiggy@gmail.com>
2021-06-29 08:35:19 -0500

    updated

--- src/components/main.js
+++ src/components/main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './css/main.css'
 //images
 import about from './images/image-about-dark.jpg';
+import about_light from './images/image-about-light.jpg';
+
 const Main = () => {
 	return <main>
 		        <div>
 		            <img src={about} alt="table" />
 		        </div>
-		        <div>
+		        <div className="about_article">
 		            <h2>About our furniture</h2>
 		            <p>Our multifunctional collection blends design and function to suit your individual taste.
 		                Make each room unique, or pick a cohesive theme that best express your interests and what
@@ -15,7 +17,7 @@ const Main = () => {
 		                or anything in between. Product specialists are available to help you create your dream space.</p>
 		        </div>
 		        <div>
-		            <img src="" alt="" />
+		            <img src={about_light} alt="taable light" />
 		        </div>
     		</main>
 }
